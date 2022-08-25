8e0902dbe4418c2a386ad1d2480286bd9fc9373c
Jonas Vannieuwenhuijsen <jonas.vannieuwenhuijsen@student.uhasselt.be>
2022-08-25 11:53:54 +0200

    team table migration

---
+++ rest-api/database/migrations/2022_08_25_083948_create_teams_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('pokemon_1')->nullable();;
+            $table->integer('pokemon_2')->nullable();;
+            $table->integer('pokemon_3')->nullable();;
+            $table->integer('pokemon_4')->nullable();;
+            $table->integer('pokemon_5')->nullable();;
+            $table->integer('pokemon_6')->nullable();;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+};
