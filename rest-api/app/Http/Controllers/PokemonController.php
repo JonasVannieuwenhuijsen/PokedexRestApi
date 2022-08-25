673a046fce27a021ffdd913f049b9a115e8234aa
Jonas Vannieuwenhuijsen <jonas.vannieuwenhuijsen@student.uhasselt.be>
2022-08-25 10:00:55 +0200

    Create PokemonController with logic

--- rest-api/app/Http/Controllers/PokemonController.php
+++ rest-api/app/Http/Controllers/PokemonController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Pokemon;
+use Illuminate\Support\Facades\DB;
 
 class PokemonController extends Controller
 {
@@ -12,37 +13,54 @@ class PokemonController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         //All pokemons
 
+        $response = [];
+        $typesArray = [];
+
         $pokemons = Pokemon::all();
 
+        $sort = $request->input('sort');
+
+        
+
+        if($sort == 'id-desc'){
+            $pokemons = $pokemons->sortByDesc('id');
+        }elseif($sort == 'name-asc'){
+            $pokemons = $pokemons->sortBy('name');
+        }elseif($sort == 'name-desc'){
+            $pokemons = $pokemons->sortByDesc('name');
+        } elseif($sort == 'id-asc'){
+            $pokemons = $pokemons->sortBy('id');
+        }
+
+        foreach ($pokemons as $pokemon){
+            $sprite_front_default = DB::table('sprites')->where('pokemon_id', $pokemon['id'])->value('front_default');
+
+            $types = DB::table('types')->where('pokemon_id', $pokemon['id'])->get();
+            foreach($types as $type){
+                $newType = array("slot" => $type->slot, "type" => array("name" => $type->type));
+                array_push($typesArray, $newType);
+            }
+
+            $newPokemon = array( 
+                "id" => $pokemon['id'],
+                "name" => $pokemon['name'],
+                "sprites" => array(
+                    "front_default" => $sprite_front_default),
+                "types" => $typesArray,
+              ); 
+
+              array_push($response, $newPokemon);
+              $typesArray = [];
+        }
+
         return response()->json([
-            'pokemons' => $pokemons
+            "pokemons" => $response
          ],200);
 
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
     }
 
     /**
@@ -53,40 +71,143 @@ class PokemonController extends Controller
      */
     public function show($id)
     {
-        //
+
+        $typesArray = [];
+        $movesArray = [];
+        $versionGroupDetailsArray = [];
+        $statsArray = [];
+        $abilitiesArray = [];
+
+        $pokemon = Pokemon::find($id);
+
+        //dd($pokemon);
+        if($pokemon == null){
+            return response()->json([
+                "error" => "ERROR",
+                "error_message" => "Pokemon not found"
+             ],404);
+        }else{
+            $sprites = DB::table('sprites')->where('pokemon_id', $pokemon['id'])->first();
+
+            $types = DB::table('types')->where('pokemon_id', $pokemon['id'])->get();
+            foreach($types as $type){
+                $newType = array("slot" => $type->slot, "type" => array("name" => $type->type));
+                array_push($typesArray, $newType);
+            }
+    
+            $moves = DB::table('moves')->where('pokemon_id', $pokemon['id'])->get();
+            foreach($moves as $move){
+    
+                $versionGroupDetails = DB::table('version_group_details')->where('move_id', $move->id)->get();
+                foreach($versionGroupDetails as $versionGroupDetail){
+                    $newVersionGroupDetail = array("move_learn_method" => $versionGroupDetail->move_learn_method, 
+                                                   "version_group" => $versionGroupDetail->version_group,
+                                                   "level_learned_at" => $versionGroupDetail->level_learned_at);
+                    array_push($versionGroupDetailsArray, $newVersionGroupDetail);
+                }    
+    
+                $newMove = array("move" => $move->move, "version_group_details" => $versionGroupDetailsArray);
+                array_push($movesArray, $newMove);
+                $versionGroupDetailsArray = [];
+            }
+    
+            $stats = DB::table('stats')->where('pokemon_id', $pokemon['id'])->get();
+            foreach($stats as $stat){
+                $statType = array("stat" => $stat->stat,
+                                  "base_stat" => $stat->base_stat,
+                                  "effort" => $stat->effort);
+                array_push($statsArray, $statType);
+            }
+    
+            $abilities = DB::table('abilities')->where('pokemon_id', $pokemon['id'])->get();
+            foreach($abilities as $ability){
+                $newAbility = array("ability" => $ability->ability,
+                                    "is_hidden" => $ability->is_hidden,
+                                    "slot" => $ability->slot);
+                array_push($abilitiesArray, $newAbility);
+            }
+    
+            $newPokemon = array( 
+                "id" => $pokemon['id'],
+                "name" => $pokemon['name'],
+                "sprites" => array(
+                    "front_default" => $sprites->front_default,
+                    "front_female" => $sprites->front_female,
+                    "front_shiny" => $sprites->front_shiny,
+                    "front_shiny_female" => $sprites->front_shiny_female,
+                    "back_default" => $sprites->back_default,
+                    "back_female" => $sprites->back_female,
+                    "back_shiny" => $sprites->back_shiny,
+                    "back_shiny_female" => $sprites->back_shiny_female),
+                "types" => $typesArray,
+                "height" => $pokemon['height'],
+                "weight" => $pokemon['weight'],
+                "moves" => $movesArray,
+                "order" => $pokemon['order'],
+                "species" => $pokemon['species'],
+                "stats" => $statsArray,
+                "abilities" => $abilitiesArray,
+                "form" => $pokemon['form']
+                ); 
+            
+    
+            return response()->json([
+                "pokemons" => $newPokemon
+             ],200);
+        }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function search(Request $request)
     {
-        //
-    }
+        //All pokemons
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+        $response = [];
+        $typesArray = [];
+        $resultIDsArray = [];
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $nameOrType = $request->input('query');
+        $limit = intval($request->input('limit'));
+
+        $pokemonIDByName = DB::table('pokemon')->where('name', $nameOrType)->value('id'); //all pokemons have unique names
+        if($pokemonIDByName != null){
+            array_push($resultIDsArray, $pokemonIDByName);
+        }
+
+        $pokemonsIDByType = DB::table('types')->where('type', $nameOrType)->get();
+        foreach ($pokemonsIDByType as $pokemonIDByType){
+            array_push($resultIDsArray, $pokemonIDByType->pokemon_id);
+        }
+
+        if($limit == null){
+            $limit = count($resultIDsArray);
+        }
+
+        for ($x = 0; $x < $limit; $x++){
+            $pokemonById = DB::table('pokemon')->where('id', $resultIDsArray[$x])->first();
+
+            $sprite_front_default = DB::table('sprites')->where('pokemon_id', $pokemonById->id)->value('front_default');
+
+            $types = DB::table('types')->where('pokemon_id', $pokemonById->id)->get();
+            foreach($types as $type){
+                $newType = array("slot" => $type->slot, "type" => array("name" => $type->type));
+                array_push($typesArray, $newType);
+            }
+
+            $newPokemon = array( 
+                "id" => $pokemonById->id,
+                "name" => $pokemonById->name,
+                "sprites" => array(
+                    "front_default" => $sprite_front_default),
+                "types" => $typesArray,
+              ); 
+
+              array_push($response, $newPokemon);
+              $typesArray = [];
+        }
+
+        return response()->json([
+            "pokemons" => $response
+         ],200);
+
     }
 }
